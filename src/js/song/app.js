7811127c58725158f0b4640857d6c59767c34320
ranranqi <1137625794@qq.com>
2018-12-12 01:45:08 +0800

    播放页面获取歌曲信息 并 加audio播放音乐

---
+++ src/js/song/app.js
@@ -0,0 +1,55 @@
+{
+    let view = {
+        el: '#app',
+        template: `
+            <audio autoplay controls src={{url}}></audio>
+        `,
+        render(data){
+            $(this.el).html(this.template.replace('{{url}}',data.url))
+        }
+    }
+    let model = {
+        data: {
+            id: '',
+            name: '',
+            singer: '',
+            url: ''
+        },
+        get(id){
+            var query = new AV.Query('Song')
+            return query.get(id).then((song )=>{
+                Object.assign(this.data, {id: song.id, ...song.attributes})
+                return song 
+            })
+        }
+    }
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            let id = this.getSongId()
+            this.model.get(id).then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        getSongId(){
+            let search = window.location.search
+            if(search.indexOf('?') === 0){
+                search = search.substring(1)
+            }
+            let array = search.split('&').filter(v=>v)
+            let id = ''
+            for(let i=0;i<array.length;i++){
+                let kv = array[i].split('=')
+                let key = kv[0]
+                let value = kv[1]
+                if(key === 'id'){
+                    id = value
+                    break
+                }
+            }
+            return id
+        }
+    }
+    controller.init(view,model)
+}
