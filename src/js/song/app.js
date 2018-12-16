ee9ecf29ff0fc1d7957916952e1d4a851923dc3d
ranranqi <1137625794@qq.com>
2018-12-16 15:38:05 +0800

    添加歌词滚动

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -13,6 +13,9 @@
           audio.onended = ()=>{
             window.eventHub.emit('songEnd')
           }
+          audio.ontimeupdate = ()=>{
+            this.showLyric(audio.currentTime)
+          }
         }
         if(status === 'playing'){
           this.$el.find('.disc-container').addClass('playing')
@@ -23,9 +26,46 @@
         let {lyrics} = song
         let array = lyrics.split('\n').map((string)=>{
           let p = document.createElement('p')
+          let regex = /\[([\d:.]+)\](.+)/
+          let matches =string.match(regex)
+          console.log(matches)
+          if(matches){
+            p.textContent = matches[2]
+            let time = matches[1]
+            let parts = time.split(':')
+            let minutes = parts[0]
+            let seconds = parts[1]
+            let newTime = parseInt(minutes,10) * 60 + parseFloat(seconds,10)
+            p.setAttribute('data-time', newTime)
+          }else{
           p.textContent = string
+        }
           this.$el.find('.lyric > .lines').append(p)
         })
+      },
+      showLyric(time){
+        let allP = this.$el.find('.lyric>.lines>p')
+        let p 
+        for(let i =0;i<allP.length;i++){
+          if(i===allP.length-1){
+            p = allP[i]
+            break
+          }else{
+            let currentTime = allP.eq(i).attr('data-time')
+            let nextTime = allP.eq(i+1).attr('data-time')
+            if(currentTime <= time && time < nextTime){
+              p = allP[i]
+              break
+            }
+          }
+        }
+        let pHeight = p.getBoundingClientRect().top
+        let linesHeight = this.$el.find('.lyric>.lines')[0].getBoundingClientRect().top
+        let height = pHeight - linesHeight
+        this.$el.find('.lyric>.lines').css({
+          transform: `translateY(${- (height - 25)}px)`
+        })
+        $(p).addClass('active').siblings('.active').removeClass('active')
       },
       play(){
         this.$el.find('audio')[0].play()
